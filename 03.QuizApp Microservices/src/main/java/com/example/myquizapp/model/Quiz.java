4dd9dd31ed0b2617b49c0a741bebc3edd656f0c0
KedarKK1 <officialkedark1@gmail.com>
2023-08-27 17:58:30 +0530

    created microservices folder, copied monolithic proj. from 01.QuizApp Monolithic

---
+++ 03.QuizApp Microservices/src/main/java/com/example/myquizapp/model/Quiz.java
@@ -0,0 +1,48 @@
+package com.example.myquizapp.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.Data;
+
+@Data
+@Entity
+public class Quiz {
+	
+	// as lombok.data is not working, i had to use source > gettersAndSetters here
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public List<Question> getQuestions() {
+		return questions;
+	}
+
+	public void setQuestions(List<Question> questions) {
+		this.questions = questions;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Integer id;
+	public String title;
+	
+	@ManyToMany
+	private List<Question> questions;
+}
---
+++ 03.QuizApp Microservices/src/test/java/com/example/myquizapp/QuizAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myquizapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
